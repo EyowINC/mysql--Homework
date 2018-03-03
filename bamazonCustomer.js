024a79ea945052e8d240dd155f2b08bb9ee08427
Abdi Abdikarin <eyowinc@gmail.com>
2018-03-03 16:48:44 -0600

    code updated

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,5 +1,14 @@
-var mysql = require("mysql");
-var prompt = require("prompt");
+//pseudocode my steps.
+ // Creating Database 
+ // Creating js Node file
+ // Query mydata
+ // add to data
+ // show all my products 
+
+
+// Mysql lets you connect to databse. read more about npm package. 
+var mysql = require("mysql"); 
+var inquirer = require("inquirer");
 
 var connection = mysql.createConnection({
   host: "localhost",
@@ -11,7 +20,51 @@ var connection = mysql.createConnection({
 
 connection.connect(function(err) {
   if (err) throw err;
-  console.log();
-  createProduct();
-});
+  console.log("My js file is now connected to the database YAY!");
+  
+  inquirer
+  .prompt([
+    {
+      name: "item",
+      type: "input",
+      message: "What is the item you would like to buy?"
+    },
+    {
+      name: "category",
+      type: "input",
+      message: "how many you like to buy?"
+    }
+    
+  ])
+  .then(function(answer) {
+    console.log(answer);
+showallproducts();
 
+});
+})
+
+  // when finished prompting, insert a new item into the db with that info
+  // connection.query(
+  //   "INSERT INTO auctions SET ?",
+  //   {
+  //     item_name: answer.item,
+  //     category: answer.category,
+  //     starting_bid: answer.startingBid,
+  //     highest_bid: answer.startingBid
+  //   },
+  //   function(err) {
+  //     if (err) throw err;
+  //     console.log("Your auction was created successfully!");
+  //     // re-prompt the user for if they want to bid or post
+  //     start();
+  //   }
+  // );
+
+function showallproducts(){
+  
+  connection.query('SELECT * FROM products', function(err, res){
+    if (err)throw err;
+    console.log(res);
+    connection.end();
+  });
+};
